b3db0b758fabd5979f6fe0d11061cbfec44d045b
Aubrey Neimeier <neimeier.1@osu.edu>
2018-09-05 22:40:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function getRepositories() {
   req.open('GET', 'https://api.github.com/users/octocat/repos');
   req.send();
 }
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  Handlebars.registerPartial(
+    'authorPartial',
+    document.getElementById('author-partial-template').innerHTML
+  );
+});
