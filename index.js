a64e7f019c985a2c11103fa780d8132eb56b8289
Aubrey Neimeier <neimeier.1@osu.edu>
2018-09-05 22:40:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ function getRepositories() {
   req.send();
 }
 
+function showRepositories(event, data) {
+  const repos = JSON.parse(this.responseText);
+  const src = document.getElementById('repository-template').innerHTML;
+  const template = Handlebars.compile(src);
+  const repoList = template(repos);
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
 document.addEventListener('DOMContentLoaded', function(event) {
   Handlebars.registerPartial(
     'authorPartial',
